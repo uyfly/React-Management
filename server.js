0c9c10c4b78322ac631f7bdf8a516e81ba2bdaa9
wooyoung.kim <uyfly@naver.com>
2023-10-01 12:15:28 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,33 +7,24 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+
+connection.connect();
+
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: "kwy120",
-      image: "https://picsum.photos/64/64",
-      name: "김우영",
-      birthday: "920120",
-      gender: "남자",
-      job: "회사원",
-    },
-    {
-      id: "skyelly96",
-      image: "https://picsum.photos/64/64",
-      name: "권민아",
-      birthday: "990102",
-      gender: "여자",
-      job: "비서",
-    },
-    {
-      id: "tonabii",
-      image: "https://picsum.photos/64/64",
-      name: "지성온",
-      birthday: "961212",
-      gender: "남자",
-      job: "대학생",
-    },
-  ]);
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
