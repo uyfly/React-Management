a8783fcf97f527e49f1be7b974756f79805b282d
wooyoung.kim <uyfly@naver.com>
2023-10-01 23:41:39 +0900

    Update UI/UX & Add Customer Search Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,73 @@
 import { useEffect, useState } from "react";
 import {
+  AppBar,
+  Box,
+  IconButton,
+  InputBase,
+  Paper,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
   TableRow,
-  Paper,
+  Toolbar,
+  Typography,
   CircularProgress,
 } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
 import "./App.css";
 import Customer from "./components/Customer";
 import CustomerAdd from "./components/CustomerAdd";
+import { styled, alpha } from "@mui/material/styles";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
 
 function App() {
   const [customers, setCustomers] = useState();
   const [completed, setCompleted] = useState(0);
+  const [searchKeyword, setSearchKeyword] = useState("");
 
   useEffect(() => {
     let timer = setInterval(progress, 20);
@@ -25,6 +77,9 @@ function App() {
   }, []);
 
   const stateRefresh = () => {
+    setCustomers([]);
+    setCompleted(0);
+    setSearchKeyword("");
     callApi()
       .then((res) => setCustomers(res))
       .catch((err) => console.log(err));
@@ -40,26 +95,79 @@ function App() {
     setCompleted((prev) => (prev >= 100 ? 0 : prev + 1));
   };
 
+  const cellList = [
+    "번호",
+    "프로필 이미지",
+    "이름",
+    "생년월일",
+    "성별",
+    "직업",
+    "설정",
+  ];
+
+  const handleSearchKeyword = (e) => {
+    setSearchKeyword(e.target.value);
+  };
+
+  const filteredComponents = (data) => {
+    data = data.filter((c) => {
+      return c.name.indexOf(searchKeyword) > -1;
+    });
+    return data.map((c) => {
+      return <Customer stateRefresh={stateRefresh} key={c.id} customer={c} />;
+    });
+  };
+
   return (
     <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            >
+              고객 관리 시스템
+            </Typography>
+            <CustomerAdd stateRefresh={stateRefresh} />
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="검색하기"
+                inputProps={{ "aria-label": "search" }}
+                name="searchKeyword"
+                value={searchKeyword}
+                onChange={handleSearchKeyword}
+              />
+            </Search>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>프로필</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-              <TableCell>설정</TableCell>
+              {cellList.map((c) => {
+                return <TableCell>{c}</TableCell>;
+              })}
             </TableRow>
           </TableHead>
           <TableBody>
             {customers ? (
-              customers.map((c) => (
-                <Customer key={c.id} customer={c} stateRefresh={stateRefresh} />
-              ))
+              filteredComponents(customers)
             ) : (
               <TableRow>
                 <TableCell colSpan={6} align="center">
@@ -70,7 +178,6 @@ function App() {
           </TableBody>
         </Table>
       </TableContainer>
-      <CustomerAdd stateRefresh={stateRefresh} />
     </>
   );
 }
